4b30474d2cd398bc270080de52dfc53282092475
Saba1234567280 <nakashidzesaba41@gmail.com>
2024-05-02 11:23:23 +0400

    twenty-sixth commit

---
+++ day 26/hom work/lesson26.py
@@ -0,0 +1,25 @@
+# 2) მომხმარებელს შეეკითხეთ სწავლობს თუარა გოაში, თუ სწავლობს შეეკითხეთ ომელ ჯგუფშია, თუ პასუხი იქნება ჯგუფი13 შეეკითეთ რომ არის თუ არა გაბრიელი მისი მენტორი, თუ პასუხი იქნება კი უთხარით რომ თქვენც აქ სწავლობთ და ნამდვილად გაბრიელია ორივეს მენტორი, თუ პასუხი არიქნება გაბრიელი ყველა სხვა შემთხვევაში დაუბეჭდეთ რომ ის ტყუის და არარის სინამდვილეში ჯგუფ13-
+
+
+user = input("swavlob GOA_shi? : ")
+
+if user == "ki":
+    print("dzalian kargia rom goashi swavlob!")
+else:
+    print("cudia aucileblad shemodi goashi!")
+user_grup = input("romel jgufgshi xar?: ")
+if user_grup == 13:
+    print("mec jguf 13_shi var")
+else:
+    print("me ar var mag jgufshi ")
+tech = input("sheni mentori batoni gabrielia?")
+if tech == "ki":
+    print("Chemi memntoric batoni gabrielia. shen namdviald swavlob goashi da es kargia:)")
+else:
+    print("shen ar swavlob goashi dzalian cudia:(  aucileblad shemouertdi goas! ")
+    
+
+
+
+
+
