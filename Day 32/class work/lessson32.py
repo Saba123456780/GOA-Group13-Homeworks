a273e6e8cd18767da3c19ee64a2d44f8c594ab33
Saba1234567280 <nakashidzesaba41@gmail.com>
2024-05-23 20:09:22 +0400

    commit

---
+++ Day 32/class work/lessson32.py
@@ -0,0 +1,29 @@
+# toys = ["bunny", "teddy", "spuerel"]
+# print(toys[1])
+
+# info = ["cat", "dog"]
+# academy = "goa best"
+# print(info[0] + info[1])
+
+# შემქმენით ცვლადისადაც იქნება წინადადება "Goa is best" შეეცადეთ 
+# ეს წინანდადება შეართოთ ერთად და დაპრინტოთ ერთ ხაზში
+
+# goa = "goa is best"
+
+# new_srting = ""
+
+# for i in range(12):
+#     (new_srting)
+
+
+# for i in range(11):
+    # result += 
+
+GOA = "goa is best"
+
+result = ""
+
+for i in range (11):
+    result += GOA[i]
+   
+print(result)
---
+++ Day 32/home work/lessson32.py
@@ -0,0 +1,25 @@
+# 2) გაანალიზეთ, დღევანდელი გაკვეთილი და დაწერეთ მსგავსი მაგალითი თქვნით,
+
+academy = "Goa ari chad akademia"
+
+aca = " "
+
+for i in range(21):
+    aca += academy[i]
+
+print(aca)
+
+
+# 3) დღევანდელი გაკვეთილის მსგავსად შემქენით ცვალდი "goa is the best"
+# ფორ ციკლის მეშვეობით და დღეს ნასწავლი ინდექსაციით შეართეთ  "goa is the best" სფეისების გარეშე
+
+Goa = "goa is the best" 
+
+result = " "
+
+for i in range(len(Goa)):
+    if Goa[i] !=" ":
+        result += Goa[i]
+
+
+print(result)
