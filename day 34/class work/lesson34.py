5460054c6bd77d02a2eb8dd62b9cc697cc3cb2dc
Saba1234567280 <nakashidzesaba41@gmail.com>
2024-05-29 16:36:42 +0400

    commit

---
+++ day 34/class work/lesson34.py
@@ -0,0 +1,7 @@
+goachadpeople = [ "batoni luka", "batoni nika", "batoni gabrieli" , "batoni davit", "gio", "davit gorgassali", "fanauriss xxinkali", "shilaki", "jemala",]
+
+
+# start , end ssstep.
+ 
+
+print(goachadpeople[0:1])
---
+++ day 34/home ork/lesson34.py
@@ -0,0 +1,28 @@
+#   2) შექმნათ სია სადაც ჩაამატებთ თქვენი ყველა ჯგუფელის სახელს და გვარს, შემდეგ კი დაბეჭდავთ შუა 10 მოსწავლეს 
+
+name_uername = ["Aleko Tirkia", "Anastasia Khvedelidze", "Andria Alavidze", "Andria Iakobishvili", "Abdria Korchilava", "Andria samsonidze", "Andria shanava", "Barbare Tinikashvili",
+                "Beridze Mate", "Dachi sazandrishvili", "Data Tezelashvili", "Dato Berkacashvili", "Dato miribiani", 
+                 "Davit grdelashvili", "Davit janashia", "Deme Biganashvili", "Erekle Karagozishvili", "Gabriel Gvinashvili",
+                "gio Alada", "Giorgi Vanishvili", "Giviko Kavrihvili", "Guram Vakhtangashvili", 
+                 "Guri Jishkariani", "Ilia iklauri", "Lasha Lomidze", "Leqso Leverashvili", "Luka Chakhvashvili", "Luka Luka", "Luka natroshvili", "luka Navrozashvili",
+                "Luumi Zurabiani", "Masho Lo", "Mate Giorgelashvili", "Mate Kotorashvili",
+                 "Mate rekhviashvili", "Mate Tolordava", "Mekaluashvili Andria", " Nika Vhunashvili", "Nika Nachepia", "Nika shavade", "Nikiolas Ndiaye", "Nikoloz Filishvili", 
+                 "Nikoloz Nutsubidze", "Nikoloz Qimeride", "Qristine Kiknadze", 
+                 "saba kiblashvili", "saba Lomsadze", 
+                "sandro chapidze", "sandro Zabaidze", "Tazo kldiaVhvili", "Toma Omanashvili", "Vika Gvilia", "Zura Beridze", "Kichashvili Nika", "Aleqsandre soadze", "Bibilashvili Giorgi",
+                "Gega diasamidze", "Giorgi qareli", "Davit gigilashvili",
+                "Data diasamidze", "Dato tyeshelashvili", "Datuna Cixelashvili", "Zardiashvili mariami", "Mishiko Berelidze", "Niko keweymadze", "Nikoloz Kukava", 
+                "Nikoloz mirziashili", "Nikoloz Ninua", "Nikusha Kublashvili"] 
+
+print(name_uername[35:45])
+
+
+# 3) შექმენით სია რომელსაც გადასცემთ ანბანის ყველა ასოს, შემდეგ თქვენ ამ სიაში უნდა იპოვოთ g, o,  და a მოახდინოთ ამ სამი ასოს კონკატინაცია ანუ შეერთება და დაბეჭდოთ goa
+
+Alphabet = ["a", "b", "g", "d", "e", "v", "z", "t", "i", "k", "l", "m", 
+            "n", "o", "p", "j", "r", "s", "t", "u", "f" , "q", "R", "y",
+            "sh", "c", "dz", "w", "W", "x", "j", "h"]
+
+Goa = Alphabet[2] + Alphabet[13] + Alphabet[0]
+
+print(Goa)
