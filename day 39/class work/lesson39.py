f2325cd75f4aa66c506ff9b1501469c4fb392302
Saba1234567280 <nakashidzesaba41@gmail.com>
2024-06-16 11:39:10 +0400

    commit

---
+++ day 39/class work/lesson39.py
@@ -0,0 +1,24 @@
+initial_price = int(input("enter product price: "))
+
+discount_percentage = 20
+
+discount_amount = initial_price * discount_percentage / 100
+
+new_price = initial_price - discount_amount
+
+print("new price is", new_price)
+
+
+
+kenti = []
+luwi = []
+
+number = int(input("enter number: "))
+
+if number % 2 != 0:
+    kenti.append(number)
+else:
+    luwi.append(number)
+
+print("კენტების სია", kenti)
+print("ლუწების სია", luwi)
